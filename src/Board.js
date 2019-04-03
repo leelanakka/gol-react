c0b7273f1365d068d71d7c1d1a70b9f436ffd0bd
Nakka LeelaPrasanth <44019459+leelanakka@users.noreply.github.com>
2019-04-03 23:16:23 +0530

    extracted the methods of board class to outside
    using total board itself to make things easier
    now mostly  the game is working

--- src/Board.js
+++ src/Board.js
@@ -1,38 +1,100 @@
 import React from "react";
+import "./index.css";
+
+const initialGrid = function(height, width) {
+  let grid = new Array(width).fill(height).map(x => new Array(x).fill(0));
+  return grid;
+};
+
+const totalAliveNeighbors = function(cell, grid) {
+  let neighbors = findingNeighbors([11, 11], cell);
+  let isAlive = checkForAlive.bind(null, grid);
+  let aliveNeighbor = neighbors.filter(isAlive);
+  return aliveNeighbor.length;
+};
+
+const checkRangeForNegativeNumbers = function(cell) {
+  return cell[0] >= 0 && cell[1] >= 0;
+};
+
+const predicate = function(cell, neighbour) {
+  return !(cell[0] === neighbour[0] && cell[1] === neighbour[1]);
+};
+
+const isCoordinatesGreaterThanBoard = function(boardSize, cell) {
+  return cell[0] < boardSize[0] && cell[1] < boardSize[1];
+};
+
+const checkForAlive = function(grid, neighbour) {
+  return grid[neighbour[0]][neighbour[1]] !== 0;
+};
+
+const cartesian = function(set1, set2) {
+  let resultSet = [];
+  for (let rowIndex = 0; rowIndex < set1.length; rowIndex++) {
+    for (let columnIndex = 0; columnIndex < set2.length; columnIndex++) {
+      resultSet.push([set1[rowIndex], set2[columnIndex]]);
+    }
+  }
+  return resultSet;
+};
+
+const validNeighbors = function(possibleNeighbors, cell, size) {
+  let validNeighbors = possibleNeighbors.filter(neighbour =>
+    predicate(cell, neighbour)
+  );
+  validNeighbors = validNeighbors.filter(checkRangeForNegativeNumbers);
+  let checkGreaterThanBoard = isCoordinatesGreaterThanBoard.bind(null, size);
+  validNeighbors = validNeighbors.filter(checkGreaterThanBoard);
+  return validNeighbors;
+};
+
+const findingNeighbors = function(size, cell) {
+  let rowCoordinates = [cell[0] - 1, cell[0], cell[0] + 1];
+  let coloumnCoordinates = [cell[1] - 1, cell[1], cell[1] + 1];
+  let possibleNeighbors = cartesian(rowCoordinates, coloumnCoordinates);
+  let neighbors = validNeighbors(possibleNeighbors, cell, size);
+
+  return neighbors;
+};
+
+const checkForNextGenration = function(currentCellState, neighbourLength) {
+  let rules = [0, 0, currentCellState, 1, 0, 0, 0, 0, 0];
+  return rules[neighbourLength];
+};
+
+const generateNextWorld = function(initialWorld) {
+  let nextWorld = initialWorld.map(x => x.slice());
+  for (let index = 0; index < initialWorld.length; index++) {
+    for (let i = 0; i < initialWorld[0].length; i++) {
+      let noOfAliveNeighbours = totalAliveNeighbors([index, i], initialWorld);
+      let nextState = checkForNextGenration(
+        initialWorld[index][i],
+        noOfAliveNeighbours
+      );
+      nextWorld[index][i] = nextState;
+    }
+  }
+  return nextWorld;
+};
 
 class Board extends React.Component {
   constructor(props) {
     super(props);
     this.size = +props.size;
+    const board = initialGrid(this.size + 1, this.size + 1);
     this.state = {
-      liveCells: [],
+      board,
       bounds: { topLeft: 0, bottomRight: this.size + 1 }
     };
   }
 
   makeCellLive(e) {
-    const id = e.target.id;
-    this.setState(state => {
-      state.liveCells.push([id]);
-    });
+    const [rowId, colId] = e.target.id.split("_");
+    const board = this.state.board.slice();
+    board[rowId][colId] = 1 - board[rowId][colId];
 
-    document.getElementById(e.target.id).style.background = "black";
-  }
-
-  createRow(rowIndex) {
-    const table = [];
-    for (let index = 0; index <= this.size; index++) {
-      table.push(
-        <td
-          key={index + Math.random()}
-          onClick={this.makeCellLive.bind(this)}
-          id={rowIndex + "_" + index}
-        >
-          1
-        </td>
-      );
-    }
-    return table;
+    this.setState({ board });
   }
 
   updateLiveCells() {
@@ -49,138 +111,39 @@ class Board extends React.Component {
     };
   }
 
-  initialGrid(height, width) {
-    let grid = new Array(width).fill(height).map(x => new Array(x).fill(" "));
-    return grid;
-  }
-
-  totalAliveNeighbors(cell, grid) {
-    let neighbors = this.findingNeighbors([this.size, this.size], cell);
-    let isAlive = this.checkForAlive.bind(null, grid);
-    let aliveNeighbor = neighbors.filter(isAlive);
-    return aliveNeighbor.length;
-  }
-
-  checkRangeForNegativeNumbers(cell) {
-    return cell[0] >= 0 && cell[1] >= 0;
-  }
-
-  predicate(cell, neighbour) {
-    return !(cell[0] === neighbour[0] && cell[1] === neighbour[1]);
-  }
-
-  isCoordinatesGreaterThanBoard(boardSize, cell) {
-    return cell[0] < boardSize[0] && cell[1] < boardSize[1];
-  }
-
-  checkForAlive(grid, neighbour) {
-    return grid[neighbour[0]][neighbour[1]] === "*";
-  }
-
-  cartesian(set1, set2) {
-    let resultSet = [];
-    for (let rowIndex = 0; rowIndex < set1.length; rowIndex++) {
-      for (let columnIndex = 0; columnIndex < set2.length; columnIndex++) {
-        resultSet.push([set1[rowIndex], set2[columnIndex]]);
-      }
-    }
-    return resultSet;
-  }
-
-  validNeighbors(possibleNeighbors, cell, size) {
-    let validNeighbors = possibleNeighbors.filter(neighbour =>
-      this.predicate(cell, neighbour)
-    );
-    validNeighbors = validNeighbors.filter(this.checkRangeForNegativeNumbers);
-    let checkGreaterThanBoard = this.isCoordinatesGreaterThanBoard.bind(
-      null,
-      size
-    );
-    validNeighbors = validNeighbors.filter(checkGreaterThanBoard);
-    return validNeighbors;
-  }
-
-  findingNeighbors(size, cell) {
-    let rowCoordinates = [cell[0] - 1, cell[0], cell[0] + 1];
-    let coloumnCoordinates = [cell[1] - 1, cell[1], cell[1] + 1];
-    let possibleNeighbors = this.cartesian(rowCoordinates, coloumnCoordinates);
-    let neighbors = this.validNeighbors(possibleNeighbors, cell, size);
-
-    return neighbors;
-  }
-
-  checkForNextGenration(currentCellState, neighbourLength) {
-    let rules = [" ", " ", currentCellState, "*", " ", " ", " ", " ", " "];
-    return rules[neighbourLength];
-  }
-
-  generateNextWorld(initialWorld) {
-    let nextWorld = initialWorld.map(x => x.slice());
-    for (let index = 0; index < initialWorld.length; index++) {
-      for (let i = 0; i < initialWorld[0].length; i++) {
-        let noOfAliveNeighbours = this.totalAliveNeighbors(
-          [index, i],
-          initialWorld
-        );
-        let nextState = this.checkForNextGenration(
-          initialWorld[index][i],
-          noOfAliveNeighbours
-        );
-        nextWorld[index][i] = nextState;
-      }
-    }
-    return nextWorld;
-  }
-
-  getAliveCellsOfNextGeneration(nextWorld, width, height) {
-    let result = [];
-    for (let rowIndex = 0; rowIndex < width; rowIndex++) {
-      for (let colomnIndex = 0; colomnIndex < height; colomnIndex++) {
-        if (nextWorld[rowIndex][colomnIndex] === "*") {
-          result.push([rowIndex, colomnIndex]);
-        }
-      }
-    }
-    return result;
-  }
-
-  generateWorld(grid, aliveCells) {
-    for (let aliveCell of aliveCells) {
-      grid[aliveCell[0].split("_")[0]][aliveCell[0].split("_")[1]] = "*";
-    }
-    return grid;
-  }
-
-  nextGeneration(currGeneration, bounds) {
-    let { height, width } = this.getDimension();
-    let grid = this.generateWorld(
-      this.initialGrid(width + 1, height + 1),
-      currGeneration
-    );
-    let nextWorld = this.generateNextWorld(grid);
-    return this.getAliveCellsOfNextGeneration(nextWorld, height, width);
-  }
-
   start() {
-    this.state.liveCells.forEach(coordinate => {
-      document.getElementById(coordinate.join("")).style.background = "red";
-    });
-    this.updateLiveCells();
+    this.timerId = setInterval(() => {
+      const board = generateNextWorld(this.state.board);
+      console.log(board);
+      this.setState({ board });
+    }, 1000);
   }
 
-  createTable() {
-    const row = [];
-    for (let index = 0; index <= this.size; index++) {
-      row.push(<tr key={index + Math.random()}>{this.createRow(index)}</tr>);
-    }
-    return row;
+  generateTable() {
+    return this.state.board.map((row, rowId) => {
+      const columns = row.map((col, colId) => {
+        let className = "alive";
+        if (col === 0) {
+          className = "dead";
+        }
+        const id = rowId + "_" + colId;
+        return (
+          <td
+            id={id}
+            className={className}
+            onClick={this.makeCellLive.bind(this)}
+          />
+        );
+      });
+      return <tr>{columns}</tr>;
+    });
   }
 
   render() {
     return (
       <div>
         <table>
-          <tbody>{this.createTable()}</tbody>
+          <tbody>{this.generateTable()}</tbody>
         </table>
         <button onClick={this.start.bind(this)}>start</button>
         <button>stop</button>
