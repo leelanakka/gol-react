f3f6743a75bd6437088e740f7ee46c21e67d9734
Nakka LeelaPrasanth <44019459+leelanakka@users.noreply.github.com>
2019-04-03 23:37:01 +0530

    Updates

---
+++ precache-manifest.81cd6687b3c3d23f1330ac8fd05c2c5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d76b19858250d90f4b42",
+    "url": "/gol-react/static/css/main.57714960.chunk.css"
+  },
+  {
+    "revision": "d76b19858250d90f4b42",
+    "url": "/gol-react/static/js/main.236bb5fc.chunk.js"
+  },
+  {
+    "revision": "d99d7a9e223364735716",
+    "url": "/gol-react/static/js/runtime~main.b14f0b41.js"
+  },
+  {
+    "revision": "0c4d940c47b748e59b1c",
+    "url": "/gol-react/static/js/2.06399952.chunk.js"
+  },
+  {
+    "revision": "a99e8a605b540e63cc945968b7492bb9",
+    "url": "/gol-react/index.html"
+  }
+];
